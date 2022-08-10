03eb65c6dbdbf8b31e6c445f29b9bcbdbae14ad3
shazin <shapalakka@gmail.com>
2022-08-10 12:40:44 +0530

    ultrasonic sensor will read distance and rotate servo motor according to it

--- Code/Automated_Dustbin_Code/Automated_Dustbin_Code.ino
+++ Code/Automated_Dustbin_Code/Automated_Dustbin_Code.ino
@@ -1,16 +1,27 @@
-#include <SR04.h>
+#include "Ultrasonic.h"
 #include <Servo.h>
 int Servo_Pin = 3;
 int uls_trig = 5;
 int uls_echo = 6;
+int ultraSonicRange = 60;//you can put value from 2 -> 400
 Ultrasonic ultrasonic(uls_trig, uls_echo);
 Servo myservo;
 
 void setup() {
+  Serial.begin(9600);
   myservo.attach(9);
 }
 
 void loop() {
-  
+  int dist = ultrasonic.distanceRead(CM);
+  Serial.println(dist);
+  if(dist < 60){
+    myservo.write(180);
+    delay(1000);
+    }
+    else{
+      myservo.write(90);
+      }
+  delay(60);
 
 }
