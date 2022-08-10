fe0e24eb074de661a501d211b72f8dc312941874
shazin <shapalakka@gmail.com>
2022-08-10 12:13:40 +0530

    initialized ultrasonic sensor

--- Code/Automated_Dustbin_Code/Automated_Dustbin_Code.ino
+++ Code/Automated_Dustbin_Code/Automated_Dustbin_Code.ino
@@ -1,6 +1,9 @@
 #include <SR04.h>
 #include <Servo.h>
-int Servo_Pin = 3
+int Servo_Pin = 3;
+int uls_trig = 5;
+int uls_echo = 6;
+Ultrasonic ultrasonic(uls_trig, uls_echo);
 Servo myservo;
 
 void setup() {
