62956e81efcfe867978c8423ad7375669c220851
shazin <shapalakka@gmail.com>
2022-08-16 17:34:12 +0530

    version 2.0

--- Automated_Dustbin_Code/Automated_Dustbin_Code.ino
+++ Automated_Dustbin_Code/Automated_Dustbin_Code.ino
@@ -1,8 +1,8 @@
 #include "Ultrasonic.h"
 #include <Servo.h>
 int Servo_Pin = 3;
-int uls_trig = 5;
-int uls_echo = 6;
+int uls_trig = 4;
+int uls_echo = 2;
 int ultraSonicRange = 60;//you can put value from 2 -> 400
 Ultrasonic ultrasonic(uls_trig, uls_echo);
 Servo myservo;
@@ -10,13 +10,14 @@ Servo myservo;
 void setup() {
   Serial.begin(9600);
   myservo.attach(Servo_Pin);
+  myservo.write(90);
 }
 
 void loop() {
   int dist = ultrasonic.distanceRead(CM);
   Serial.println(dist);
   if(dist < 60){
-    myservo.write(180);
+    myservo.write(-180);
     delay(1000);
     }
     else{
